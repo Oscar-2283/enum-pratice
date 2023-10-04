77936da0ad4c55d17f3e8a25de1b0f7d6da94165
Oscar <93570814+Oscar-2283@users.noreply.github.com>
2023-10-04 13:23:41 +0800

    Update users.ts
    666

--- src/model/users/users.ts
+++ src/model/users/users.ts
@@ -17,64 +17,64 @@ export function parseUsersData(data: any) {
   return tranformData.parse(data);
 }
 
-export function parseConsultantScheduleList(data: any) {
-  const tranformData = z.object({
-    consultant_user_id: z.number(),
-    hb_consultant_id: z.number(),
-    name: z.string().nullable(),
-    sort_name: z.string(),
-    timeslots: z.array(
-      z.object({
-        created_at: z.string(),
-        hb_consultant_id : z.number(),
-        id: z.number(),
-        status: z.number(),
-        timeslot: z.number(),
-        timestamp: z.number(),
-        type: z.string()
-      })
-    ).nullable(), 
-  }).transform(deepCamelCaseKeys);
+// export function parseConsultantScheduleList(data: any) {
+//   const tranformData = z.object({
+//     consultant_user_id: z.number(),
+//     hb_consultant_id: z.number(),
+//     name: z.string().nullable(),
+//     sort_name: z.string(),
+//     timeslots: z.array(
+//       z.object({
+//         created_at: z.string(),
+//         hb_consultant_id : z.number(),
+//         id: z.number(),
+//         status: z.number(),
+//         timeslot: z.number(),
+//         timestamp: z.number(),
+//         type: z.string()
+//       })
+//     ).nullable(), 
+//   }).transform(deepCamelCaseKeys);
   
-  const ConsultantScheduleList = z.array(tranformData);
-  return ConsultantScheduleList.parse(data);
-}
+//   const ConsultantScheduleList = z.array(tranformData);
+//   return ConsultantScheduleList.parse(data);
+// }
 
-export function parseConsultantWeeklyScheduleList(data: any) {
-  const tranformData = z.object({
-    date: z.string(),
-    statistics: z.array(
-      z.object({
-        hbf_total: z.number(),
-        hbt_total : z.number(),
-        timeslot: z.number(),
-        timestamp: z.string(),
-      })
-    ), 
-  }).transform(deepCamelCaseKeys);
+// export function parseConsultantWeeklyScheduleList(data: any) {
+//   const tranformData = z.object({
+//     date: z.string(),
+//     statistics: z.array(
+//       z.object({
+//         hbf_total: z.number(),
+//         hbt_total : z.number(),
+//         timeslot: z.number(),
+//         timestamp: z.string(),
+//       })
+//     ), 
+//   }).transform(deepCamelCaseKeys);
   
-  const ConsultantWeeklyScheduleList = z.array(tranformData);
-  return ConsultantWeeklyScheduleList.parse(data);
-}
+//   const ConsultantWeeklyScheduleList = z.array(tranformData);
+//   return ConsultantWeeklyScheduleList.parse(data);
+// }
 
-export function parseGetWeeklyScheduleConsultants(data: any) {
-  const tranformData = z.array(
-      z.object({
-        consultant_user_id: z.number(),
-        email : z.string(),
-        grade: z.string(),
-        hb_consultant_name: z.string(),
-        is_demo: z.number(),
-        max_level: z.number(),
-        min_level: z.number(),
-        nationality: z.string(),
-        picture: z.string().nullable(),
-        seven_days: z.number(),
-        work_phone: z.string(),
-      })
-    ) 
-  .transform(deepCamelCaseKeys);
+// export function parseGetWeeklyScheduleConsultants(data: any) {
+//   const tranformData = z.array(
+//       z.object({
+//         consultant_user_id: z.number(),
+//         email : z.string(),
+//         grade: z.string(),
+//         hb_consultant_name: z.string(),
+//         is_demo: z.number(),
+//         max_level: z.number(),
+//         min_level: z.number(),
+//         nationality: z.string(),
+//         picture: z.string().nullable(),
+//         seven_days: z.number(),
+//         work_phone: z.string(),
+//       })
+//     ) 
+//   .transform(deepCamelCaseKeys);
   
-  const getWeeklyScheduleConsultants = tranformData;
-  return getWeeklyScheduleConsultants.parse(data);
-}
+//   const getWeeklyScheduleConsultants = tranformData;
+//   return getWeeklyScheduleConsultants.parse(data);
+// }
